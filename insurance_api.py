654511c5ca940cf12677d5fe17444c5546f8c3b8
premklp16 <premsarangan@gmail.com>
2025-08-07 19:40:45 +0530

    Added import time

--- insurance_api.py
+++ insurance_api.py
@@ -13,6 +13,7 @@ import pdf_to_text
 import Embed_the_text_file
 import clear_files
 import os
+import time
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
